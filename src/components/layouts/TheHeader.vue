b7cae48fcbe65f292240b86a60d2b9c4523b194a
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-24 15:02:51 +0200

    setup

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <img src="../../assets/sudokuLogo.png" alt="Sudoku" />
+    <h1>Sudoku - Vue 3</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #0072e3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+}
+
+img {
+  height: 30px;
+}
+
+h1 {
+  margin-left: 1rem;
+  color: white;
+}
+</style>
